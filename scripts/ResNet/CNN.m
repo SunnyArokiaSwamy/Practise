aa4197c61d63f27db0467b29fc7b0affd1ffd37b
Sunny Bellary <sunny.bellary@hitachi-automotive.us>
2018-08-19 21:50:18 -0400

    CNN working for all data S2 train and S1 test 78.2%

--- scripts/ResNet/CNN.m
+++ scripts/ResNet/CNN.m
@@ -7,12 +7,12 @@ addpath(genpath('../CNN/'));
 loadConstants;
 
 % Sunny Inputs
-% subject_numb = [1 2 3 4 5 6];
+subject_num = [1 2 3 4 5 6];
 % sessions = [1 2];
 
-subject_num = [1 3];
+%subject_num = [1 3];
 event_type = [6,9];
-electrodes = [47];
+electrodes = [47 48];
 E = []; N=[];
 session_num = 2;
 for i=1:length(subject_num)
@@ -20,8 +20,8 @@ for i=1:length(subject_num)
     filt_EEG = loadforFiltering(Raw_EEG,f);% must repair a bit
     separatedData = eventSpecificData(filt_EEG, event,event_type);
     [Errp, normal] = ReshapeData(separatedData,electrodes);  
-    figure(i),subplot(2,1,1), plot(mean(cell2mat(Errp)));
-    figure(i),subplot(2,1,2), plot(mean(cell2mat(normal)));
+%     figure(i),subplot(2,1,1), plot(mean(cell2mat(Errp)));
+%     figure(i),subplot(2,1,2), plot(mean(cell2mat(normal)));
     E = [E; Errp];
     N = [N; normal];
 end
@@ -30,7 +30,7 @@ YTrain = [ones(length(E),1); 2*ones(length(E),1)];
 
 
 E = []; N=[];
-session_num = 2;
+session_num = 1;
 for i=1:length(subject_num)
     [Raw_EEG, event] = loadDataGivenSessionSubjectNumb(subject_num(i),session_num);
     filt_EEG = loadforFiltering(Raw_EEG,f);% must repair a bit
@@ -43,7 +43,6 @@ XTest = [E; N(1:length(E))];
 YTest = [ones(length(E),1); 2*ones(length(E),1)];
 
 
-return
 
 % For CNN
 for i=1:length(XTrain)
@@ -63,7 +62,7 @@ XTrain = XTra(:,:,:,Rd1);
 YTrain = categorical(YTrain(Rd1));
 XTest =  XTes(:,:,:,Rd2);
 YTest = categorical(YTest(Rd2));
-
+%%
 layers = [
     imageInputLayer([2 512 1])
     
@@ -88,11 +87,11 @@ layers = [
     classificationLayer];
 
 options = trainingOptions('sgdm', ...
-    'MaxEpochs',4, ...
-    'Verbose',false, ...
-    'Plots','training-progress');
-net = trainNetwork(XTrain,layers,options);
-YPred = classify(net,XTrain);
-accuracy = sum(YPred == YTrain)/numel(YTrain)
+    'MaxEpochs',100, ...
+    'Verbose',true, ...
+    'Plots','training-progress','ExecutionEnvironment','cpu');
+net = trainNetwork(XTrain,YTrain,layers,options);
+YPred = classify(net,XTest,'ExecutionEnvironment','cpu');
+accuracy = sum(YPred == YTest)/numel(YTest)
 
 
